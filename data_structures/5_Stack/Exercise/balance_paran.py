aa75de9633f70e12c631f565947875632599af9e
xena717 <46336994+xena717@users.noreply.github.com>
2022-01-03 15:30:20 -0800

    Update balance_paran.py
    my solution version #1.

--- data_structures/5_Stack/Exercise/balance_paran.py
+++ data_structures/5_Stack/Exercise/balance_paran.py
@@ -1,3 +1,4 @@
+#added Chengfeng's solution at the end
 from collections import deque
 
 class Stack:
@@ -41,6 +42,30 @@ def is_balanced(s):
 
     return stack.size()==0
 
+
+if __name__ == '__main__':
+    print(is_balanced("({a+b})"))
+    print(is_balanced("))((a+b}{"))
+    print(is_balanced("((a+b))"))
+    print(is_balanced("((a+g))"))
+    print(is_balanced("))"))
+    print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
+
+# Below is Chengfeng's solution. I tested and it worked.    
+def is_balanced(s):
+    dict={"{":"}","(":")","[":"]"}
+    stack=Stack()
+    push_count=0
+    for char in s:
+        if char in dict.keys():
+            stack.push(char)
+            push_count+=1
+        elif char in dict.values() and stack.size()!=0:
+            stack.pop()
+    if push_count!=0 and stack.size()==0:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     print(is_balanced("({a+b})"))
